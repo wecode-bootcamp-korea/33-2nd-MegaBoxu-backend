545406dd6d2adafb7505f383fe782afce6ee4617
Brian-with <chiyoungv@gmail.com>
2022-06-09 19:33:44 +0900

    Add : MovieListView
    [details]
    - movies.views.py : (Add) MovieListView
    - movies.tests.py : (Add) MovieListViewTest

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,127 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from .models import *
+
+class MovieListViewTest(TestCase):
+    
+    def setUp(self):
+        Movie.objects.bulk_create([
+            Movie(
+                id               = 1,
+                title            = "브로커",
+                poster_url       = "google.com",
+                description      = "movie description 1",
+                reservation_rate = 31.50,
+                age_limit        = 12,
+                release_date     = '2022-06-08',
+                total_viewer     = 152364,
+                running_time     = '01:01:00'
+            ),
+            Movie(
+                id               = 2,
+                title            = "범죄도시2",
+                poster_url       = "naver.com",
+                description      = "movie description 2",
+                reservation_rate = 20.5,
+                age_limit        = 15,
+                release_date     = '2030-01-02',
+                total_viewer     = 9575022,
+                running_time     = '01:02:00'
+            ),
+            Movie(
+                id               = 3,
+                title            = "기라티나와 하늘의 꽃다발",
+                poster_url       = "kakao.com",
+                description      = "movie description 3",
+                reservation_rate = 13.9,
+                age_limit        = 0,
+                release_date     = '2023-01-03',
+                total_viewer     = 397784,
+                running_time     = '01:03:00'
+            )
+        ])
+    
+    def tearDown(self):
+        Movie.objects.all().delete()
+
+    def test_success_movielist_get(self):
+        
+        client = Client()
+        response = client.get('/movie')
+        
+        self.assertEqual(response.json(),
+            {
+                'result' : [
+                    {
+                        'movie_id'        : 1,
+                        'title'           : "브로커",
+                        'poster_url'      : "google.com",
+                        'description'     : "movie description 1",
+                        'reservation_rate': '31.50',
+                        'age_limit'       : 12,
+                        'release_date'    : "2022.06.08",
+                    },
+                    {
+                        'movie_id'        : 2,
+                        'title'           : "범죄도시2",
+                        'poster_url'      : "naver.com",
+                        'description'     : "movie description 2",
+                        'reservation_rate': '20.50',
+                        'age_limit'       : 15,
+                        'release_date'    : "2030.01.02",
+                    },
+                    {
+                        'movie_id'        : 3,
+                        'title'           : "기라티나와 하늘의 꽃다발",
+                        'poster_url'      : "kakao.com",
+                        'description'     : "movie description 3",
+                        'reservation_rate': '13.90',
+                        'age_limit'       : 0,
+                        'release_date'    : "2023.01.03",
+                    }
+                ]
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_success_movielist_filter_by_sort_realesed(self):
+
+        client = Client()
+        response = client.get('/movie?sort=released')
+
+        self.assertEqual(response.json(),
+            {
+                'result' : [
+                    {
+                        'movie_id'        : 1,
+                        'title'           : "브로커",
+                        'poster_url'      : "google.com",
+                        'description'     : "movie description 1",
+                        'reservation_rate': '31.50',
+                        'age_limit'       : 12,
+                        'release_date'    : "2022.06.08",
+                    },
+                ]
+            })
+        self.assertEqual(response.status_code, 200)
+
+    def test_success_movielist_filter_by_search(self):
+
+        client = Client()
+        response = client.get('/movie?search=하늘')
+
+        self.assertEqual(response.json(),
+            {
+                'result' : [
+                    {
+                        'movie_id'        : 3,
+                        'title'           : "기라티나와 하늘의 꽃다발",
+                        'poster_url'      : "kakao.com",
+                        'description'     : "movie description 3",
+                        'reservation_rate': '13.90',
+                        'age_limit'       : 0,
+                        'release_date'    : "2023.01.03",
+                    },
+                ]
+            })
+        self.assertEqual(response.status_code, 200)
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+from django.db.models import Q
+
+from .models import Movie
+
+class MovieListView(View):
+
+    def get(self, request):
+                
+        sort   = request.GET.get("sort", None)
+        offset = int(request.GET.get("offset", 0))
+        limit  = int(request.GET.get("limit", 20))
+        search = request.GET.get("search", None)
+        q = Q()
+
+        if sort == 'released':
+            q.add(Q(release_date__lte=datetime.now().date()), q.AND)
+
+        if search :
+            q.add(Q(title__contains=search), q.AND)
+             
+        movies = Movie.objects.filter(q).order_by('-reservation_rate')[offset:limit]
+        result = [{
+            'movie_id'        : movie.id,
+            'title'           : movie.title,
+            'poster_url'      : movie.poster_url,
+            'description'     : movie.description,
+            'reservation_rate': movie.reservation_rate,
+            'age_limit'       : movie.age_limit,
+            'release_date'    : movie.release_date.strftime("%Y.%m.%d"),
+        } for movie in movies]
+
+        return JsonResponse({'result' : result}, status=200)
