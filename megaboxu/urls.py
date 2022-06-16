62cd9871b144a8aeb24a5b222e7db3adf59d4f64
박준형 <unknown>
2022-06-16 19:27:02 +0900

    수정 : 카카오 로그인

--- megaboxu/urls.py
+++ megaboxu/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path,include
 
 urlpatterns = [
-  path('users', include('users.urls')),
-  path('movies', include('movies.urls')),
-  path('reservations', include('reservations.urls')),
-  # path('reviews', include('reviews.urls'))
+  path('user', include('users.urls')),
+  path('movie', include('movies.urls')),
+  path('reservation', include('reservations.urls')),
+  path('review', include('reviews.urls'))
 ]
